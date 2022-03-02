07a1f38e95d9a9b1440f900d5e3efd17b8cc627c
Wilson Yan <wilson1.yan@berkeley.edu>
2022-03-02 17:37:09 +0000

    Fixed google drive downloading for pretrained models

--- videogpt/download.py
+++ videogpt/download.py
@@ -1,29 +1,11 @@
 import requests
 from tqdm import tqdm
 import os
+import gdown
 import torch
 
 from .vqvae import VQVAE
 from .gpt import VideoGPT
-
-
-def get_confirm_token(response):
-    for key, value in response.cookies.items():
-        if key.startswith('download_warning'):
-            return value
-    return None
-
-
-def save_response_content(response, destination):
-    CHUNK_SIZE = 8192
-
-    pbar = tqdm(total=0, unit='iB', unit_scale=True)
-    with open(destination, 'wb') as f:
-        for chunk in response.iter_content(CHUNK_SIZE):
-            if chunk:
-                f.write(chunk)
-                pbar.update(len(chunk))
-    pbar.close()
 
 
 def download(id, fname, root=os.path.expanduser('~/.cache/videogpt')):
@@ -33,16 +15,7 @@ def download(id, fname, root=os.path.expanduser('~/.cache/videogpt')):
     if os.path.exists(destination):
         return destination
 
-    URL = 'https://drive.google.com/uc?export=download'
-    session = requests.Session()
-
-    response = session.get(URL, params={'id': id}, stream=True)
-    token = get_confirm_token(response)
-
-    if token:
-        params = {'id': id, 'confirm': token}
-        response = session.get(URL, params=params, stream=True)
-    save_response_content(response, destination)
+    gdown.download(id=id, output=destination, quiet=False)
     return destination
 
 
