e94f7ae4af818ecb8e0d57d62538ad76e1f7a072
Douwe den Blanken <douwedb@gmail.com>
2024-02-09 10:01:01 +0100

    Make sure there is a real test-train split in the moving MNIST hdf5 dataset

--- scripts/create_moving_mnist_hdf5_dataset.py
+++ scripts/create_moving_mnist_hdf5_dataset.py
@@ -4,9 +4,17 @@ from torchvision.datasets import MovingMNIST
 
 import h5py
 
-split_ratio=19
+split_ratio = 20
+val_percentage = 0.05
 
-dataset = MovingMNIST(root='./datasets', split="train", download=True, split_ratio=split_ratio)
+dataset = MovingMNIST(root='../datasets', split=None, download=True)
+
+# Split the dataset into training and validation
+n_samples = len(dataset)
+n_val = int(val_percentage * n_samples)
+
+train_dataset = dataset.data[:n_samples-n_val]
+val_dataset = dataset.data[n_samples-n_val:]
 
 # Write the Moving MNIST dataset to a HDF5 file with the following format, without using .cumulative_sizes
 #     {
@@ -16,22 +24,22 @@ dataset = MovingMNIST(root='./datasets', split="train", download=True, split_rat
 #         'test_idx': [B'], np.int64
 #     }
 #     where B is the number of training videos, H and W are the height and width of the video frames, and 3 is the number of channels (RGB).
-with h5py.File('datasets/moving_mnist.h5', 'w') as f:
+with h5py.File('../datasets/moving_mnist.h5', 'w') as f:
     # Flatten along index and time dimensions for train_data
-    train_data = dataset.data.view(-1, 64, 64, 1).numpy()
+    train_data = train_dataset.view(-1, 64, 64, 1).numpy()
     # Duplicate single gray value axis to 3 channels
 
     train_data = np.repeat(train_data, 3, axis=-1)
-    train_idx = np.arange(0, len(dataset) * split_ratio, split_ratio)
+    train_idx = np.arange(0, len(train_dataset) * split_ratio, split_ratio)
     f.create_dataset('train_data', data=train_data)
     f.create_dataset('train_idx', data=train_idx)
 
     # NOTE: for now, both train and test data are the same
 
     # Flatten along index and time dimensions for test_data
-    test_data = dataset.data.view(-1, 64, 64, 1).numpy()
+    test_data = val_dataset.view(-1, 64, 64, 1).numpy()
     # Duplicate single gray value axis to 3 channels
     test_data = np.repeat(test_data, 3, axis=-1)
-    test_idx = np.arange(0, len(dataset) * split_ratio, split_ratio)
+    test_idx = np.arange(0, len(val_dataset) * split_ratio, split_ratio)
     f.create_dataset('test_data', data=test_data)
     f.create_dataset('test_idx', data=test_idx)
